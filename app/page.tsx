6fae82026c7effc0333d3cd9c7a6247bfd320ef8
daxantz <daxantz@gmail.com>
2025-05-18 19:06:26 -0400

    created component for header text

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Header from "@/components/Header";
+import HeaderText from "@/components/HeaderText";
 
 export default function Home() {
   return (
     <>
       <Header />
+      <HeaderText />
     </>
   );
 }
