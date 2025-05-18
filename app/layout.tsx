413838b26f83f2c781f81fc14ecb4aa40b0730d0
daxantz <daxantz@gmail.com>
2025-05-18 18:15:24 -0400

    created header of portfolio page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Space_Grotesk } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const spaceGrotesk = Space_Grotesk({
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  weight: ["400", "700"],
 });
 
 export const metadata: Metadata = {
@@ -24,10 +19,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body className={`${spaceGrotesk.className} antialiased`}>
+        <main>{children}</main>
       </body>
     </html>
   );
---
+++ components/Header.tsx
@@ -0,0 +1,27 @@
+import Image from "next/image";
+import React from "react";
+import LinkedIn from "@/public/LinkedIn.svg";
+import github from "@/public/github.svg";
+import githubprofile from "@/public/githubprofile.png";
+import { FileUser } from "lucide-react";
+
+const Header = () => {
+  return (
+    <div className="mx-[100px] relative  bg-gradient-to-b from-[#24242400] to-[#242424] ">
+      <div className=" mx-auto max-w-[154px] mt-[20px] flex flex-col gap-5">
+        <p className="text-center max-w-[129px] mx-auto font-bold text-2xl">
+          Dax Davis
+        </p>
+        <div className="flex justify-evenly ">
+          <Image src={LinkedIn} alt="linkedin icon" />
+          <Image src={github} alt="Github icon" />
+          <FileUser />
+        </div>
+      </div>
+
+      <Image src={githubprofile} alt="Dax Davis" />
+    </div>
+  );
+};
+
+export default Header;
