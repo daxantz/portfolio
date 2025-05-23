5efbd9363128ff05b3985c0cc60118e676952062
daxantz <daxantz@gmail.com>
2025-05-23 17:54:30 -0400

    created a header component for medium screens and larger

---
+++ components/LargerHeader.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import HeaderNav from "./HeaderNav";
+import HeaderText from "./HeaderText";
+import Image from "next/image";
+import githubprofile from "@/public/githubprofile.png";
+const LargerHeader = () => {
+  return (
+    <div className="hidden md:block">
+      <HeaderNav />
+
+      <div className="flex">
+        <HeaderText />
+        <div className=" flex flex-col justify-end w-[40%]  bg-gradient-to-b from-[#24242400] to-[#242424] ">
+          <Image src={githubprofile} alt="Dax Davis" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LargerHeader;
